1d82f1bb971373816f4a5311062c89e48f6e40c5
wmeister-pr <meister.wilhelm@hotmail.de>
2024-12-26 18:54:36 +0100

    vscodeedu.py added

---
+++ Python/vscodeedu.py
@@ -0,0 +1,17 @@
+print(len("Hello world"))
+print("hello world title".title().count("o"))
+print()
+echo = "To be or" + " NOT to be"[:6]
+print(echo)
+print(echo.count("o"))
+print()
+echo = "To be or" + " NOT to be"
+print(echo)
+print(echo[:6])
+print(echo[6:])
+print(echo[:6].count("o"))
+
+print("You enter a number adn I will tell you how many 1s are in it.")
+print(input("Enter a number: ").count("1"))
+
+input("Press Enter to continue...")
